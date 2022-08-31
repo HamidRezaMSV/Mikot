c01c3b419d3d865480e043034be885459201e6bb
Hamid Reza Mousavi <hamidreza.m799@gmail.com>
2022-08-31 20:31:53 +0430

    complete usecase module

---
+++ app/src/main/java/hamid/msv/mikot/di/UseCaseModule.kt
@@ -0,0 +1,56 @@
+package hamid.msv.mikot.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hamid.msv.mikot.domain.repository.MessageRepository
+import hamid.msv.mikot.domain.repository.UserRepository
+import hamid.msv.mikot.domain.usecase.*
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideCreateNewMessageUseCase(messageRepository: MessageRepository) =
+        CreateNewMessageUseCase(messageRepository)
+
+    @Provides
+    @Singleton
+    fun provideGetAllMessagesUseCase(messageRepository: MessageRepository) =
+        GetAllMessagesUseCase(messageRepository)
+
+    @Provides
+    @Singleton
+    fun provideGetAllUsersUseCase(userRepository: UserRepository) =
+        GetAllUsersUseCase(userRepository)
+
+    @Provides
+    @Singleton
+    fun provideGetChatsLastMessageUseCase(messageRepository: MessageRepository) =
+        GetChatsLastMessageUseCase(messageRepository)
+
+    @Provides
+    @Singleton
+    fun provideSaveUserInDatabaseUseCase(userRepository: UserRepository) =
+        SaveUserInDatabaseUseCase(userRepository)
+
+    @Provides
+    @Singleton
+    fun provideSignInUserUseCase(userRepository: UserRepository) =
+        SignInUserUseCase(userRepository)
+
+    @Provides
+    @Singleton
+    fun provideSignUpUserUseCase(userRepository: UserRepository) =
+        SignUpUserUseCase(userRepository)
+
+    @Provides
+    @Singleton
+    fun provideUpdateChatLastMessageUseCase(messageRepository: MessageRepository) =
+        UpdateChatLastMessageUseCase(messageRepository)
+
+}
