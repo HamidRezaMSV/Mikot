76a901e4ed29ca775bd110c4f0783b422192f493
Hamid Reza Mousavi <hamidreza.m799@gmail.com>
2022-09-02 16:03:04 +0430

    start implementing RegisterViewModel

--- app/src/main/java/hamid/msv/mikot/presentation/screen/register/RegisterViewModel.kt
+++ app/src/main/java/hamid/msv/mikot/presentation/screen/register/RegisterViewModel.kt
@@ -1,11 +1,32 @@
 package hamid.msv.mikot.presentation.screen.register
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import hamid.msv.mikot.domain.usecase.SaveLoginStateUseCase
+import hamid.msv.mikot.domain.usecase.SaveUserInDatabaseUseCase
+import hamid.msv.mikot.domain.usecase.SignUpUserUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class RegisterViewModel : ViewModel() {
+class RegisterViewModel @Inject constructor(
+    private val signUpUserUseCase: SignUpUserUseCase,
+    private val saveUserInDatabaseUseCase: SaveUserInDatabaseUseCase ,
+    private val saveLoginStateUseCase: SaveLoginStateUseCase
+): ViewModel() {
 
+//    val signUpResponse : StateFlow<Task<AuthResult>> = signUpUserUseCase.response.value
 
+    val signUpResponse = mutableStateOf(signUpUserUseCase.response.value)
+
+    fun signUpUser(email:String , password : String){
+        viewModelScope.launch(Dispatchers.IO) { signUpUserUseCase.execute(email, password) }
+    }
 
 }
