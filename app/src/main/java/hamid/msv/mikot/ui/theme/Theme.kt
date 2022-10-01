1658023867fb0ebe27f23eb1846c0d26d47f02be
Hamid Reza Mousavi <hamidreza.m799@gmail.com>
2022-10-02 02:25:20 +0330

    change statusBarColor to White

--- app/src/main/java/hamid/msv/mikot/ui/theme/Theme.kt
+++ app/src/main/java/hamid/msv/mikot/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -27,13 +29,29 @@ private val LightColorPalette = lightColors(
     */
 )
 
+
 @Composable
 fun MikotTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
+
+    val systemUiController = rememberSystemUiController()
+    if(darkTheme){
+        systemUiController.setStatusBarColor(
+            color = Color.White,
+            darkIcons = true
+        )
+    }else{
+        systemUiController.setStatusBarColor(
+            color = Color.White,
+            darkIcons = true
+        )
+    }
+
 
     MaterialTheme(
         colors = colors,
